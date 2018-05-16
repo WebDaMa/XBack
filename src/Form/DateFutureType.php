d45e280d4c37c03217660c01c216c89d5089092c
Danny Matthijs <bloodgpotato@gmail.com>
2018-05-16 17:58:40 +0200

    no time

--- src/Form/DateFutureType.php
+++ src/Form/DateFutureType.php
@@ -4,10 +4,10 @@
 namespace App\Form;
 
 
-use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-class DateFutureType extends DateTimeType {
+class DateFutureType extends DateType {
 
     public function configureOptions(OptionsResolver $resolver)
     {
@@ -17,7 +17,5 @@ class DateFutureType extends DateTimeType {
         // Override: Go back 20 years and add 20 years
         $resolver->setDefault('years', range(date('Y') - 20, date('Y') + 20));
 
-        // Override: Use an hour range between 08:00AM and 11:00PM
-        $resolver->setDefault('hours', range(8, 23));
     }
 }

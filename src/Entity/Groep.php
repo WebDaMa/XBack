8fd7cc42b8680377e173e82f09bbb7b5841d3994
Danny Matthijs <bloodgpotato@gmail.com>
2018-07-04 23:30:14 +0200

    rename

--- src/Entity/Groep.php
+++ src/Entity/Groep.php
@@ -21,7 +21,7 @@ class Groep extends TypeTimestamps
 
     /**
      * @ORM\OneToMany(targetEntity="App\Entity\Customer", mappedBy="groupLayout")
-     * @ORM\OrderBy({"first_name" = "ASC"})
+     * @ORM\OrderBy({"firstName" = "ASC"})
      */
     private $groupCustomers;
 

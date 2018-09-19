927df4148c89eba6e153720855ab5cf38d57d281
Danny Matthijs <bloodgpotato@gmail.com>
2018-09-19 17:09:37 +0200

    new unique func

--- src/Logic/Extensions.php
+++ src/Logic/Extensions.php
@@ -6,18 +6,7 @@ namespace App\Logic;
 
 class Extensions {
 
-    public static function unique_multidim_array($array, $key) {
-        $temp_array = array();
-        $i = 0;
-        $key_array = array();
-
-        foreach($array as $val) {
-            if (!in_array($val[$key], $key_array)) {
-                $key_array[$i] = $val[$key];
-                $temp_array[$i] = $val;
-            }
-            $i++;
-        }
-        return $temp_array;
+    public static function unique_multidim_array($array) {
+        return array_map("unserialize", array_unique(array_map("serialize", $array)));
     }
 }

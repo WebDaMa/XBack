009c5180b72500b3c108c254aa8936c3852e24b0
Danny Matthijs <bloodgpotato@gmail.com>
2018-09-19 17:17:18 +0200

    other attempt

--- src/Logic/Extensions.php
+++ src/Logic/Extensions.php
@@ -6,7 +6,17 @@ namespace App\Logic;
 
 class Extensions {
 
-    public static function unique_multidim_array($array) {
-        return array_map("unserialize", array_unique(array_map("serialize", $array)));
+    public static function unique_multidim_array($array, $key) {
+        $temp_array = array();
+        $i = 0;
+        $key_array = array();
+        foreach( $array as $val ) {
+            if ( ! in_array( $val[$key], $key_array ) ) {
+                $key_array[$i] = $val[$key];
+                $temp_array[] = $val; // <--- remove the $i
+            }
+            $i++;
+        }
+        return $temp_array;
     }
 }

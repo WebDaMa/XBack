5d21c97e79cd34be2a32b0ac6b9a4f6412290e10
Danny Matthijs <bloodgpotato@gmail.com>
2018-09-22 19:23:13 +0200

    trim name

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -415,8 +415,8 @@ class DashboardController extends Controller {
         $guide = new Guide();
 
         $guide->setGuideShort($row[0]);
-        $guide->setGuideFirstName($row[1]);
-        $guide->setGuideLastName($row[2]);
+        $guide->setGuideFirstName(trim($row[1]));
+        $guide->setGuideLastName(trim($row[2]));
 
         return $guide;
     }

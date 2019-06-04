ee01d3f562161354bd1816bed511fa0f5b1c71b1
Danny Matthijs <bloodgpotato@gmail.com>
2019-06-04 21:00:44 +0200

    clear cache url

---
+++ src/Controller/CacheController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class CacheController extends AbstractController
+{
+    /**
+     * @Route("/cache", name="cache")
+     */
+    public function index()
+    {
+        $dir = dirname(__DIR__, 2);
+        exec("php ". $dir ."/bin/console cache:clear --env=prod");
+
+        return $this->render('cache/index.html.twig', [
+            'controller_name' => 'CacheController',
+        ]);
+    }
+}
